b4268d0fd01d837bf4b6d1a7452c69a268f46027
DrGrim <lucifron_n@yahoo.com>
2019-11-11 14:38:52 +0000

    Update azure-functions-inject.js

--- azure-functions-inject.js
+++ azure-functions-inject.js
@@ -2,6 +2,7 @@ var Hours   = 0 ;
 var Minutes = 0 ;
 var Seconds = 0 ;
 var Tracker ;
+var TrackerStatus = "Inactive";
 
 $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>").click(function(){
 
@@ -10,6 +11,7 @@ $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=
                 $('input[aria-label="Time Spent"]').parent().append("<div id=\"timer\" style=\"color:green;font-weight:bold;position:absolute;top: 0px;right: 50px;\"> 0 hour(s) : 0 minute(s) : 0 second(s) </div>");
                 $('#trackbtn').html("STOP").css('background-color','red');
 		Tracker = setInterval(StartTracking, 1000);
+		TrackerStatus = "Active";
 
         }else{
 
@@ -19,6 +21,7 @@ $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=
                 Minutes = 0;
                 Seconds = 0;
                 $('#timer').remove();
+		TrackerStatus = "Inactive";
                 
         }
 
@@ -47,3 +50,10 @@ function StartTracking(){
 
                   $('#timer').html(Hours+' hour(s) : '+Minutes+' minute(s) : '+Seconds+' second(s)');  
 }
+
+$(window).on('beforeunload', function() {
+     if(TrackerStatus == "Active"){
+     
+	 return "Time tracking in effect ! If you decide to move from this page time tracking will be stopped . If you don't want to stop time tracking for this item please open your new url in a new window !";
+     }
+});
