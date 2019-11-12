2db4968506bc84412291e4b05a6396bb2a029e8f
DrGrim <lucifron_n@yahoo.com>
2019-11-12 11:43:42 +0000

    Update azure-functions-inject.js

--- azure-functions-inject.js
+++ azure-functions-inject.js
@@ -3,132 +3,133 @@ var StartDate ;
 
 $( document ).ready(function() {
 
- $('input[aria-label="Time Spent"]').attr('disabled','disabled');
- $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>");
+	$('input[aria-label="Time Spent"]').attr('disabled','disabled');
+	$('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>");
 
-$('#trackbtn').click(function(){ 
-	      if($('#trackbtn').html().indexOf('START') > -1  ){
-                       
-			if(StartDate == null){
-		       
-			    setCookie($('span[aria-label="ID Field"]').html());
-			    $('.work-item-form-title > div > div > input').val($('.work-item-form-title > div > div > input').val() + " ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)").change();
-			    setTimeout(function(){ $('.bowtie-save').parent().click();     }, 1000);
-				
-		       }
-			   Start();
+	$('#trackbtn').click(function(){ 
+		
+	    if($('#trackbtn').html().indexOf('START') > -1  ){
 
-		}else{
-			
-			   deleteCookie($('span[aria-label="ID Field"]').html());
-			   $('.work-item-form-title > div > div > input').val(function(i, v) {return v.replace(" ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)","");}).change();
-			   setTimeout(function(){ $('.bowtie-save').parent().click(); setTimeout(function(){ $('#speshalnote').focus(); }, 1000);  }, 1000);
-			   Stop();
+		 if(StartDate == null){
+
+		      setCookie($('span[aria-label="ID Field"]').html());
+		      $('.work-item-form-title > div > div > input').val($('.work-item-form-title > div > div > input').val() + " ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)").change();
+		      setTimeout(function(){ $('.bowtie-save').parent().click();     }, 1000);
 
 		}
+		Start();
 
-});
+	    }else{
 
-$('body').on('keypress', '#speshalnote', function(){
+		      deleteCookie($('span[aria-label="ID Field"]').html());
+		      $('.work-item-form-title > div > div > input').val(function(i, v) {return v.replace(" ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)","");}).change();
+		      setTimeout(function(){ $('.bowtie-save').parent().click(); setTimeout(function(){ $('#speshalnote').focus(); }, 1000);  }, 1000);
+		      Stop();
 
-     var key = window.event.keyCode;
-     if (key === 13 && $('#speshalnote').val().length == 0) {
-        
-        //post comment here with just the time spent
-        $('#optionalNote').remove();
+	    }
 
-     }
+	});
 
-});
+	$('body').on('keypress', '#speshalnote', function(){
 
-function Start(){
+	     	var key = window.event.keyCode;
+	     	if (key === 13 && $('#speshalnote').val().length == 0) {
+
+			//post comment here with just the time spent
+			$('#optionalNote').remove();
+
+	        }
+
+	});
+
+	function Start(){
+
+		$('input[aria-label="Time Spent"]').parent().append("<div id=\"timer\" style=\"font-size:11px;color:green;font-weight:bold;position:absolute;top: 3px;right: 50px;\"> 0 hour(s) : 0 minute(s) : 0 second(s) </div>");
+		$('#trackbtn').html("STOP").css('background-color','red');
+		Tracker = setInterval(duration, 1000);
+
+	}
 	
-	$('input[aria-label="Time Spent"]').parent().append("<div id=\"timer\" style=\"font-size:11px;color:green;font-weight:bold;position:absolute;top: 3px;right: 50px;\"> 0 hour(s) : 0 minute(s) : 0 second(s) </div>");
-	$('#trackbtn').html("STOP").css('background-color','red');
-	Tracker = setInterval(duration, 1000);
+	function Stop(){
+
+		 $('#trackbtn').html("START").css('background-color','green');
+		 $('#timer').remove();
+		 clearInterval(Tracker);
+		 OptionalNote();
+
+	}
 	
-}
+	function OptionalNote(){
+
+		$('body').append(' <div id="optionalNote" style="position:fixed;top:200px;left:45%;background-color:#fff;box-shadow: 0px 0px 5px black;width:auto;height:auto;padding: 10px;"><div style="pointer-events: none;text-align:center;">Any notes to add ?</div><div style="margin-top:10px;"><textarea id="speshalnote" style="margin: 0px; width: 429px; height: 149px;"></textarea><br><button value="Submit Note" id="submitNote" style="float: right;border: none;background-color: #0077d4;color: #fff;padding: 5px;font-size: 12px;cursor:pointer;">Submit Note</button></div>  </div>  ');
+		setDraggable('#optionalNote');
+
+	}
+
+	function setCookie(itemid) {
+
+		var d = new Date();
+		StartDate = d.getTime();
+		d.setTime(d.getTime() + (3600 * 1000 * 24 * 365 * 10));
+		var expires = "expires="+ d.toUTCString();
+
+		document.cookie = itemid + "=" + StartDate + ";" + expires + ";path=/";
+
+	}
+
+	function deleteCookie(itemid) {
+
+		document.cookie = itemid + "=" + StartDate + "; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+		StartDate = null;
+
+	}
+
+	function CheckForStartCookie(itemid){
+
+	     if(document.cookie.indexOf(itemid+"=") >= 0){
+
+		/*==========================Cookie===========================*/
+		//read cookie string
+		var Cookie = document.cookie.split(';');
+		//get the value
+		var TargetValue = Cookie[1].split('=');
+		//convert miliseconds to an actual date
+		var StampedFullDate = new Date( parseInt(TargetValue[1])).getTime();
+		StartDate = new Date(StampedFullDate);
+
+		$('#trackbtn').click();
+
+	     }
+
+	}
 	
-function Stop(){
+        //this function is not complete 	
+        function duration() {
 
-	 $('#trackbtn').html("START").css('background-color','green');
-	 $('#timer').remove();
-	 clearInterval(Tracker);
-	 OptionalNote();
+
+		var d = new Date();
+		// get total seconds between the times
+		var delta = Math.abs(StartDate - d) / 1000;
+
+		// calculate (and subtract) whole days
+		var days = Math.floor(delta / 86400);
+		delta -= days * 86400;
+
+		// calculate (and subtract) whole hours
+		var hr = Math.floor(delta / 3600) % 24;
+		delta -= hr * 3600;
+
+		// calculate (and subtract) whole minutes
+		var min = Math.floor(delta / 60) % 60;
+		delta -= min * 60;
+
+		// what's left is seconds
+		var sec = delta % 60;  // in theory the modulus is not required
+
+		$('#timer').html(hr+' hour(s) : '+min+' minute(s) : '+sec.toFixed(0)+' second(s)');
+        }
 	
-}
-	
-function OptionalNote(){
-
-	$('body').append(' <div id="optionalNote" style="position:fixed;top:200px;left:45%;background-color:#fff;box-shadow: 0px 0px 5px black;width:auto;height:auto;padding: 10px;"><div style="pointer-events: none;text-align:center;">Any notes to add ?</div><div style="margin-top:10px;"><textarea id="speshalnote" style="margin: 0px; width: 429px; height: 149px;"></textarea><br><button value="Submit Note" id="submitNote" style="float: right;border: none;background-color: #0077d4;color: #fff;padding: 5px;font-size: 12px;cursor:pointer;">Submit Note</button></div>  </div>  ');
-	setDraggable('#optionalNote');
-   
-}
-
- function setCookie(itemid) {
-
-	  var d = new Date();
-	  StartDate = d.getTime();
-	  d.setTime(d.getTime() + (3600 * 1000 * 24 * 365 * 10));
-	  var expires = "expires="+ d.toUTCString();
-
-	  document.cookie = itemid + "=" + StartDate + ";" + expires + ";path=/";
-
- }
-
- function deleteCookie(itemid) {
-
-	   document.cookie = itemid + "=" + StartDate + "; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-	   StartDate = null;
-
- }
-
- function CheckForStartCookie(itemid){
-
-		if(document.cookie.indexOf(itemid+"=") >= 0){
-                 
-			/*==========================Cookie===========================*/
-			//read cookie string
-			var Cookie = document.cookie.split(';');
-			//get the value
-			var TargetValue = Cookie[1].split('=');
-			//convert miliseconds to an actual date
-			var StampedFullDate = new Date( parseInt(TargetValue[1])).getTime();
-			StartDate = new Date(StampedFullDate);
-			
-			$('#trackbtn').click();
-
-		}
-
-   }
-	
-   //this function is not complete 	
-   function duration() {
-
-
-        var d = new Date();
-        // get total seconds between the times
-        var delta = Math.abs(StartDate - d) / 1000;
-
-        // calculate (and subtract) whole days
-        var days = Math.floor(delta / 86400);
-        delta -= days * 86400;
-
-        // calculate (and subtract) whole hours
-        var hr = Math.floor(delta / 3600) % 24;
-        delta -= hr * 3600;
-
-        // calculate (and subtract) whole minutes
-        var min = Math.floor(delta / 60) % 60;
-        delta -= min * 60;
-
-        // what's left is seconds
-        var sec = delta % 60;  // in theory the modulus is not required
-
-	$('#timer').html(hr+' hour(s) : '+min+' minute(s) : '+sec.toFixed(0)+' second(s)');
-    }
-	
-    CheckForStartCookie($('span[aria-label="ID Field"]').html());
+       CheckForStartCookie($('span[aria-label="ID Field"]').html());
 
 	
 //=============================================================FORM DRAG AND DROP
