b5d54a7de2187677a369ea141674c8a50fea173e
DrGrim <lucifron_n@yahoo.com>
2019-11-12 08:53:03 +0000

    Update azure-functions-inject.js

--- azure-functions-inject.js
+++ azure-functions-inject.js
@@ -1,78 +1,61 @@
-var Hours   = 0 ;
-var Minutes = 0 ;
-var Seconds = 0 ;
 var Tracker ;
+var StartDate ;
 
 $( document ).ready(function() {
 
-	CheckForStartCookie($('span[aria-label="ID Field"]').html());
+ CheckForStartCookie($('span[aria-label="ID Field"]').html());
 
 
-	$('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>").click(function(){
+ $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>").click(function(){
 
 		if($('#trackbtn').html().indexOf('START') > -1){
 
-			$('input[aria-label="Time Spent"]').parent().append("<div id=\"timer\" style=\"color:green;font-weight:bold;position:absolute;top: 0px;right: 50px;\"> 0 hour(s) : 0 minute(s) : 0 second(s) </div>");
-			$('#trackbtn').html("STOP").css('background-color','red');
-			Tracker = setInterval(StartTracking, 1000);
 			setCookie($('span[aria-label="ID Field"]').html());
+			Start();
 
 		}else{
-
-			$('#trackbtn').html("START").css('background-color','green');
-			clearInterval(Tracker);
-			Hours = 0;
-			Minutes = 0;
-			Seconds = 0;
-			$('#timer').remove();
+			
 			deleteCookie($('span[aria-label="ID Field"]').html());
+			Stop();
 
 		}
 
-	});
+ });
 
-	function StartTracking(){
+function Start(){
+	
+	$('input[aria-label="Time Spent"]').parent().append("<div id=\"timer\" style=\"color:green;font-weight:bold;position:absolute;top: 0px;right: 50px;\"> 0 hour(s) : 0 minute(s) : 0 second(s) </div>");
+	$('#trackbtn').html("STOP").css('background-color','red');
+	Tracker = setInterval(duration, 1000);
+	
+}
+	
+function Stop(){
 
-			  //seconds progression 
-			  if(Seconds < 59){
+	 $('#trackbtn').html("START").css('background-color','green');
+	 $('#timer').remove();
+	 clearInterval(Tracker);
+	
+}
 
-			      Seconds++;
-
-			  }else{
-
-			       Seconds = 0;
-			       Minutes++;
-
-			  }
-
-			  if(Minutes > 59){
-
-			      Minutes = 0;
-			      Hours++;
-
-			  }
-
-			  $('#timer').html(Hours+' hour(s) : '+Minutes+' minute(s) : '+Seconds+' second(s)');  
-	}
-
-	function setCookie(itemid) {
+ function setCookie(itemid) {
 
 	  var d = new Date();
-	  var WhenDidItStart = d.getTime();
+	  StartDate = d.getTime();
 	  d.setTime(d.getTime() + (3600 * 1000 * 24 * 365 * 10));
 	  var expires = "expires="+ d.toUTCString();
 
-	  document.cookie = itemid + "=" + WhenDidItStart + ";" + expires + ";path=/";
+	  document.cookie = itemid + "=" + StartDate + ";" + expires + ";path=/";
 
-	}
+ }
 
-	function deleteCookie(itemid) {
+ function deleteCookie(itemid) {
 
 	   document.cookie = "username="+itemid+"; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-	}
+ }
 
-	function CheckForStartCookie(itemid){
+ function CheckForStartCookie(itemid){
 
 		if(document.cookie.indexOf(itemid+"=") >= 0){
                  
@@ -84,19 +67,17 @@ $( document ).ready(function() {
 			//convert miliseconds to an actual date
 			var StampedFullDate = new Date( parseInt(TargetValue[1])).getTime();
 			var date = new Date(StampedFullDate);
-			console.log(StampedFullDate);
-			/*==========================Get Time Difference===============*/
-			date_past = new Date(parseInt(TargetValue[1]));
-
+			
+			$('#trackbtn').click();
 		        //console.log(date.toString());
-			duration(StampedFullDate);
+			//duration(StampedFullDate);
 
 		}
 
-	}
+   }
 	
-    //this function is not complete 	
-    function duration(startDate) {
+   //this function is not complete 	
+   function duration() {
 
 
         var d = new Date();
@@ -118,7 +99,7 @@ $( document ).ready(function() {
         // what's left is seconds
         var sec = delta % 60;  // in theory the modulus is not required
 
-         console.log(hr+ ":"+min+":"+sec.toFixed(0));
+	$('#timer').html(hr+' hour(s) : '+min+' minute(s) : '+sec.toFixed(0)+' second(s)');
     }
 });
 
