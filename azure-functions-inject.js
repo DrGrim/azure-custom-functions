b764a39dde5bebbcdbb41a3fd87f52579095f744
DrGrim <lucifron_n@yahoo.com>
2019-11-12 11:17:34 +0000

    Update azure-functions-inject.js

--- azure-functions-inject.js
+++ azure-functions-inject.js
@@ -3,33 +3,35 @@ var StartDate ;
 
 $( document ).ready(function() {
 
- $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>").click(function(){
-  
-		if($('#trackbtn').html().indexOf('START') > -1  ){
+ $('input[aria-label="Time Spent"]').attr('disabled','disabled');
+ $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>");
+
+	$('#trackbtn').click(function(){ 
+	      if($('#trackbtn').html().indexOf('START') > -1  ){
                        
 			if(StartDate == null){
 		       
-                    setCookie($('span[aria-label="ID Field"]').html());
-                    $('.work-item-form-title > div > div > input').val($('.work-item-form-title > div > div > input').val() + " ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)").change();
-                    setTimeout(function(){ $('.bowtie-save').parent().click();     }, 1000);
+			    setCookie($('span[aria-label="ID Field"]').html());
+			    $('.work-item-form-title > div > div > input').val($('.work-item-form-title > div > div > input').val() + " ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)").change();
+			    setTimeout(function(){ $('.bowtie-save').parent().click();     }, 1000);
 				
 		       }
 			   Start();
 
 		}else{
 			
-			deleteCookie($('span[aria-label="ID Field"]').html());
-			$('.work-item-form-title > div > div > input').val(function(i, v) {return v.replace(" ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)","");}).change();
-			setTimeout(function(){ $('.bowtie-save').parent().click();     }, 1000);
-			Stop();
+			   deleteCookie($('span[aria-label="ID Field"]').html());
+			   $('.work-item-form-title > div > div > input').val(function(i, v) {return v.replace(" ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)","");}).change();
+			   setTimeout(function(){ $('.bowtie-save').parent().click();     }, 1000);
+			   Stop();
 
 		}
 
- });
+           });
 
 function Start(){
 	
-	$('input[aria-label="Time Spent"]').parent().append("<div id=\"timer\" style=\"color:green;font-weight:bold;position:absolute;top: 0px;right: 50px;\"> 0 hour(s) : 0 minute(s) : 0 second(s) </div>");
+	$('input[aria-label="Time Spent"]').parent().append("<div id=\"timer\" style=\"font-size:11px;color:green;font-weight:bold;position:absolute;top: 3px;right: 50px;\"> 0 hour(s) : 0 minute(s) : 0 second(s) </div>");
 	$('#trackbtn').html("STOP").css('background-color','red');
 	Tracker = setInterval(duration, 1000);
 	
@@ -40,7 +42,14 @@ function Stop(){
 	 $('#trackbtn').html("START").css('background-color','green');
 	 $('#timer').remove();
 	 clearInterval(Tracker);
+	 OptionalNote();
 	
+}
+	
+function OptionalNote(){
+
+	$('body').append(' <div id="optionalNote" style="position:fixed;top:200px;left:45%;background-color:#fff;box-shadow: 0px 0px 5px black;width:auto;height:auto;padding: 10px;"><div style="pointer-events: none;text-align:center;">Any notes to add ?</div><div style="margin-top:10px;"><textarea style="margin: 0px; width: 429px; height: 149px;"></textarea><br><button value="Submit Note" id="submitNote" style="float: right;border: none;background-color: #0077d4;color: #fff;padding: 5px;font-size: 12px;cursor:pointer;">Submit Note</button><button value="Submit Note" id="submitNote" style="float: left;border: none;background-color: #0077d4;color: #fff;padding: 5px 20px 5px 20px;font-size: 12px;cursor:pointer;">Skip</button></div>  </div>  ');
+	setDraggable('#optionalNote');
 }
 
  function setCookie(itemid) {
@@ -107,6 +116,79 @@ function Stop(){
     }
 	
     CheckForStartCookie($('span[aria-label="ID Field"]').html());
+
+	
+//=============================================================FORM DRAG AND DROP
+var active = false;
+var currentX;
+var currentY;
+var initialX;
+var initialY;
+var xOffset = 0;
+var yOffset = 0;
+var container = document.querySelector("body");
+
+container.addEventListener("touchstart", dragStart, false);
+container.addEventListener("touchend", dragEnd, false);
+container.addEventListener("touchmove", drag, false);
+
+container.addEventListener("mousedown", dragStart, false);
+container.addEventListener("mouseup", dragEnd, false);
+container.addEventListener("mousemove", drag, false);
+
+var dragItem ;
+
+	function setDraggable(item){
+
+	     dragItem = document.querySelector(item);
+
+	}
+
+	function dragStart(e) {
+	      if (e.type === "touchstart") {
+		initialX = e.touches[0].clientX - xOffset;
+		initialY = e.touches[0].clientY - yOffset;
+	      } else {
+		initialX = e.clientX - xOffset;
+		initialY = e.clientY - yOffset;
+	      }
+
+	      if (e.target === dragItem) {
+		active = true;
+	      }
+	}
+
+	function dragEnd(e) {
+	      initialX = currentX;
+	      initialY = currentY;
+
+	      active = false;
+	}
+
+	function drag(e) {
+	      if (active) {
+
+		e.preventDefault();
+
+		if (e.type === "touchmove") {
+		  currentX = e.touches[0].clientX - initialX;
+		  currentY = e.touches[0].clientY - initialY;
+		} else {
+		  currentX = e.clientX - initialX;
+		  currentY = e.clientY - initialY;
+		}
+
+		xOffset = currentX;
+		yOffset = currentY;
+
+		setTranslate(currentX, currentY, dragItem);
+	      }
+	}
+
+	function setTranslate(xPos, yPos, el) {
+	      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+	}
+
 });
 
 
