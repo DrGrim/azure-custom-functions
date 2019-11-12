7e434fa37fd5fbaf906ca8c8647321ba4cf50dd1
DrGrim <lucifron_n@yahoo.com>
2019-11-12 11:38:43 +0000

    Update azure-functions-inject.js

--- azure-functions-inject.js
+++ azure-functions-inject.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
  $('input[aria-label="Time Spent"]').attr('disabled','disabled');
  $('input[aria-label="Time Spent"]').parent().append("<div id=\"trackbtn\" style=\"color:#fff;background-color:green;position: absolute;top: 0px;right: 0px;padding: 3px 5px 3px 5px;text-align: center;font-weight: bold;cursor:pointer;\"> START </div>");
 
-	$('#trackbtn').click(function(){ 
+$('#trackbtn').click(function(){ 
 	      if($('#trackbtn').html().indexOf('START') > -1  ){
                        
 			if(StartDate == null){
@@ -22,12 +22,24 @@ $( document ).ready(function() {
 			
 			   deleteCookie($('span[aria-label="ID Field"]').html());
 			   $('.work-item-form-title > div > div > input').val(function(i, v) {return v.replace(" ("+ $('#mectrl_currentAccount_primary').html()  +" WIP)","");}).change();
-			   setTimeout(function(){ $('.bowtie-save').parent().click();     }, 1000);
+			   setTimeout(function(){ $('.bowtie-save').parent().click(); setTimeout(function(){ $('#speshalnote').focus(); }, 1000);  }, 1000);
 			   Stop();
 
 		}
 
-           });
+});
+
+$('body').on('keypress', '#speshalnote', function(){
+
+     var key = window.event.keyCode;
+     if (key === 13 && $('#speshalnote').val().length == 0) {
+        
+        //post comment here with just the time spent
+        $('#optionalNote').remove();
+
+     }
+
+});
 
 function Start(){
 	
@@ -48,8 +60,9 @@ function Stop(){
 	
 function OptionalNote(){
 
-	$('body').append(' <div id="optionalNote" style="position:fixed;top:200px;left:45%;background-color:#fff;box-shadow: 0px 0px 5px black;width:auto;height:auto;padding: 10px;"><div style="pointer-events: none;text-align:center;">Any notes to add ?</div><div style="margin-top:10px;"><textarea style="margin: 0px; width: 429px; height: 149px;"></textarea><br><button value="Submit Note" id="submitNote" style="float: right;border: none;background-color: #0077d4;color: #fff;padding: 5px;font-size: 12px;cursor:pointer;">Submit Note</button><button value="Submit Note" id="submitNote" style="float: left;border: none;background-color: #0077d4;color: #fff;padding: 5px 20px 5px 20px;font-size: 12px;cursor:pointer;">Skip</button></div>  </div>  ');
+	$('body').append(' <div id="optionalNote" style="position:fixed;top:200px;left:45%;background-color:#fff;box-shadow: 0px 0px 5px black;width:auto;height:auto;padding: 10px;"><div style="pointer-events: none;text-align:center;">Any notes to add ?</div><div style="margin-top:10px;"><textarea id="speshalnote" style="margin: 0px; width: 429px; height: 149px;"></textarea><br><button value="Submit Note" id="submitNote" style="float: right;border: none;background-color: #0077d4;color: #fff;padding: 5px;font-size: 12px;cursor:pointer;">Submit Note</button></div>  </div>  ');
 	setDraggable('#optionalNote');
+   
 }
 
  function setCookie(itemid) {
